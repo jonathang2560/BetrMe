af9c29ac6b3ef973ed158492f16a237e290702f7
jonathang2560 <jonathang2560@gmail.com>
2023-04-03 20:31:43 -0700

    Requirement message integration

--- js/goals.js
+++ js/goals.js
@@ -1,14 +1,27 @@
 function addGoal() {
     var goal = document.getElementById("goal").value;
     var deadline = document.getElementById("deadline").value;
+    var error = document.getElementById("error");
+  
+    if (goal.trim() === "" || deadline.trim() === "") {
+      error.style.display = "block";
+      setTimeout(function() {
+        error.style.display = "none";
+      }, 5000);
+      return;
+    }
+  
     var goalList = document.getElementById("goalList");
     var li = document.createElement("li");
-    li.innerHTML = goal + " - Deadline: " + deadline + " <button onclick='deleteGoal(this)'>Delete</button>";
+    li.innerHTML = goal + " - Deadline: " + deadline + " <button class='delete' onclick='deleteGoal(this)'>Delete</button>";
     goalList.appendChild(li);
-}
-
-function deleteGoal(item) {
+  
+    document.getElementById("goal").value = "";
+    document.getElementById("deadline").value = "";
+  }
+  
+  function deleteGoal(item) {
     var li = item.parentNode;
     li.parentNode.removeChild(li);
-}
-
+  }
+  
