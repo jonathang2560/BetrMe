b9ea95bba3f1a74e2429206796e11c3e456d9d5d
jonathang2560 <jonathang2560@gmail.com>
2023-04-03 20:45:57 -0700

    Error Update

--- js/goals.js
+++ js/goals.js
@@ -2,20 +2,21 @@ function addGoal() {
     var goal = document.getElementById("goal").value;
     var deadline = document.getElementById("deadline").value;
     var error = document.getElementById("error");
-  
+    
     if (goal.trim() === "" || deadline.trim() === "") {
-      error.style.display = "block";
-      setTimeout(function() {
-        error.style.display = "none";
-      }, 5000);
-      return;
-    }
-  
+        error.innerHTML = "Please enter a goal and a deadline.";
+        setTimeout(function() {
+          error.style.display = "none";
+        }, 5000);
+        return;
+      }
+    error.innerHTML = "";
+    
     var goalList = document.getElementById("goalList");
     var li = document.createElement("li");
     li.innerHTML = goal + " - Deadline: " + deadline + " <button class='delete' onclick='deleteGoal(this)'>Delete</button>";
     goalList.appendChild(li);
-  
+    
     document.getElementById("goal").value = "";
     document.getElementById("deadline").value = "";
   }
